6bc3c887690b15e70ec33e83a9cc8b9d08998b9b
just-hussain <1just.hussain@gmail.com>
2021-03-19 10:49:14 +0300

    improve the review flow and implement a finish

--- src/handlers/review.ts
+++ src/handlers/review.ts
@@ -19,30 +19,33 @@ let comment: string | undefined
 // The response to be populated and stored
 let response: Response
 
+let ratingBtns = () => {
+	return Markup.inlineKeyboard([
+		Markup.button.callback(i18n.__('Actions.Ratings.bad'), Actions.RATING_BAD),
+		Markup.button.callback(
+			i18n.__('Actions.Ratings.good'),
+			Actions.RATING_GOOD
+		),
+		Markup.button.callback(
+			i18n.__('Actions.Ratings.excellent'),
+			Actions.RATING_EXCELLENT
+		),
+	])
+}
+
+let nextBtn = () => {
+	return Markup.inlineKeyboard([
+		Markup.button.callback(i18n.__('Actions.next'), Actions.NEXT),
+	])
+}
+
+let homeBtn = () => {
+	return Markup.inlineKeyboard([
+		Markup.button.callback(i18n.__('Actions.home'), Actions.START),
+	])
+}
+
 export default (bot: Telegraf<MyContext>): void => {
-	let ratingBtns = () => {
-		return Markup.inlineKeyboard([
-			Markup.button.callback(
-				i18n.__('Actions.Ratings.bad'),
-				Actions.RATING_BAD
-			),
-			Markup.button.callback(
-				i18n.__('Actions.Ratings.good'),
-				Actions.RATING_GOOD
-			),
-			Markup.button.callback(
-				i18n.__('Actions.Ratings.excellent'),
-				Actions.RATING_EXCELLENT
-			),
-		])
-	}
-
-	let nextBtn = () => {
-		return Markup.inlineKeyboard([
-			Markup.button.callback(i18n.__('Actions.next'), Actions.NEXT),
-		])
-	}
-
 	// The starting point of the reviewing flow
 	bot.action(Actions.REVIEW_SERVICE, async ctx => {
 		await ctx.answerCbQuery()
@@ -84,6 +87,7 @@ export default (bot: Telegraf<MyContext>): void => {
 	bot.on('message', ctx => {
 		if (commentRequested) {
 			comment = (ctx.message as any).text
+			handleQuestionsFlow(ctx)
 		}
 	})
 
@@ -91,42 +95,42 @@ export default (bot: Telegraf<MyContext>): void => {
 	bot.action(Actions.NEXT, async ctx => {
 		await ctx.answerCbQuery()
 
-		switch (currentQuestion) {
-			case ReviewQuestions.TRACKING:
-				response.reviews.push(
-					new Review(ReviewQuestions.TRACKING, rating, comment)
-				)
-				currentQuestion = ReviewQuestions.LOCATION
-				ctx.reply(i18n.__('Prompts.Review.location'), ratingBtns())
-				break
-
-			case ReviewQuestions.LOCATION:
-				response.reviews.push(
-					new Review(ReviewQuestions.LOCATION, rating, comment)
-				)
-				currentQuestion = ReviewQuestions.STATUS
-				ctx.reply(i18n.__('Prompts.Review.status'), ratingBtns())
-				break
-
-			case ReviewQuestions.STATUS:
-				response.reviews.push(
-					new Review(ReviewQuestions.STATUS, rating, comment)
-				)
-				currentQuestion = ReviewQuestions.PRICE
-				ctx.reply(i18n.__('Prompts.Review.price'), ratingBtns())
-				break
-
-			case ReviewQuestions.PRICE:
-				response.reviews.push(
-					new Review(ReviewQuestions.PRICE, rating, comment)
-				)
-
-				// reset the questions flow
-				currentQuestion = ReviewQuestions.TRACKING
-				ctx.reply('bruh, uve finished')
-		}
-
-		commentRequested = false
-		comment = undefined
+		handleQuestionsFlow(ctx)
 	})
+}
+
+function handleQuestionsFlow(ctx: MyContext): void {
+	switch (currentQuestion) {
+		case ReviewQuestions.TRACKING:
+			response.reviews.push(
+				new Review(ReviewQuestions.TRACKING, rating, comment)
+			)
+			currentQuestion = ReviewQuestions.LOCATION
+			ctx.reply(i18n.__('Prompts.Review.location'), ratingBtns())
+			break
+
+		case ReviewQuestions.LOCATION:
+			response.reviews.push(
+				new Review(ReviewQuestions.LOCATION, rating, comment)
+			)
+			currentQuestion = ReviewQuestions.STATUS
+			ctx.reply(i18n.__('Prompts.Review.status'), ratingBtns())
+			break
+
+		case ReviewQuestions.STATUS:
+			response.reviews.push(new Review(ReviewQuestions.STATUS, rating, comment))
+			currentQuestion = ReviewQuestions.PRICE
+			ctx.reply(i18n.__('Prompts.Review.price'), ratingBtns())
+			break
+
+		case ReviewQuestions.PRICE:
+			response.reviews.push(new Review(ReviewQuestions.PRICE, rating, comment))
+
+			// reset the questions flow
+			currentQuestion = ReviewQuestions.TRACKING
+			ctx.reply(i18n.__('Prompts.Review.finish'), homeBtn())
+	}
+
+	commentRequested = false
+	comment = undefined
 }
