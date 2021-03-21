8b2e29ae82205df9065ed1c68f31fd0d55369276
just-hussain <1just.hussain@gmail.com>
2021-03-21 07:30:38 +0300

    a bit more robustness

--- src/handlers/location.ts
+++ src/handlers/location.ts
@@ -2,24 +2,29 @@ import i18n from 'i18n'
 import { Scenes, Markup } from 'telegraf'
 import { MyContext } from '../myContext'
 import { homeBtn } from '../mixins/buttons'
-import Actions from './../constants/actions'
+
+let locationRequested = false
 
 const locationRequest = () => {
 	return Markup.keyboard([
 		Markup.button.locationRequest(i18n.__('Actions.shareLocation')),
-	])
+	]).oneTime()
 }
 
 export default (bot: Scenes.BaseScene<MyContext>): void => {
 	bot.enter(async ctx => {
 		await ctx.answerCbQuery()
+		locationRequested = true
 		ctx.reply(i18n.__('Prompts.shareLocation'), locationRequest())
 	})
 
 	bot.on('location', async ctx => {
-		console.log(ctx.message)
-		ctx.session.location = { ...(ctx.message as any).location }
-		await ctx.reply(i18n.__('Prompts.gotLocation'), homeBtn())
-		ctx.scene.leave()
+		if (locationRequested) {
+			locationRequested = false
+			console.log(ctx.message)
+			ctx.session.location = { ...(ctx.message as any).location }
+			await ctx.reply(i18n.__('Prompts.gotLocation'), homeBtn())
+			ctx.scene.leave()
+		}
 	})
 }
