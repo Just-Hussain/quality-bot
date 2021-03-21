883ec3853ff20a2fc58b8d8a83f59214086d92c2
just-hussain <1just.hussain@gmail.com>
2021-03-21 09:30:08 +0300

    robustness

--- src/handlers/reports.ts
+++ src/handlers/reports.ts
@@ -9,27 +9,31 @@ export default (bot: Composer<MyContext>) => {
 		ctx.scene.leave()
 
 		let reply = `${i18n.__('Prompts.Review.header')}:\n`
-		ctx.session.responses.forEach(response => {
-			let msg = `
-			${i18n.__('Prompts.Review.tracking')}: ${response.reviews[0].rating}/3
-			${i18n.__('comment')}: ${response.reviews[0].comment || i18n.__('none')}
+		if (ctx.session.responses) {
+			ctx.session.responses.forEach(response => {
+				let msg = `
+				${i18n.__('Prompts.Review.tracking')}: ${response.reviews[0].rating}/3
+				${i18n.__('comment')}: ${response.reviews[0].comment || i18n.__('none')}
+	
+				${i18n.__('Prompts.Review.location')}: ${response.reviews[1].rating}/3
+				${i18n.__('comment')}: ${response.reviews[1].comment || i18n.__('none')}
+	
+				${i18n.__('Prompts.Review.status')}: ${response.reviews[2].rating}/3
+				${i18n.__('comment')}: ${response.reviews[2].comment || i18n.__('none')}
+	
+				${i18n.__('Prompts.Review.price')}: ${response.reviews[3].rating}/3
+				${i18n.__('comment')}: ${response.reviews[3].comment || i18n.__('none')}
+	
+				_____________
+	
+				`
+				reply += msg
+			})
 
-			${i18n.__('Prompts.Review.location')}: ${response.reviews[1].rating}/3
-			${i18n.__('comment')}: ${response.reviews[1].comment || i18n.__('none')}
-
-			${i18n.__('Prompts.Review.status')}: ${response.reviews[2].rating}/3
-			${i18n.__('comment')}: ${response.reviews[2].comment || i18n.__('none')}
-
-			${i18n.__('Prompts.Review.price')}: ${response.reviews[3].rating}/3
-			${i18n.__('comment')}: ${response.reviews[3].comment || i18n.__('none')}
-
-			_____________
-
-			`
-			reply += msg
-		})
-
-		ctx.reply(reply, homeBtn())
+			ctx.reply(reply, homeBtn())
+		} else {
+			ctx.reply('0️⃣', homeBtn())
+		}
 	})
 
 	bot.command(Commands.ISSUES, async ctx => {
@@ -38,28 +42,32 @@ export default (bot: Composer<MyContext>) => {
 		await ctx.reply(i18n.__('Prompts.Issue.header'))
 		let mediaGroup: any = []
 
-		ctx.session.issues.forEach(async issue => {
-			if (issue.photo) {
-				mediaGroup.push({
-					media: { source: `images/${issue.photo}` },
-					caption: `
-					${i18n.__('comment')}:
-					${issue.comment}
-					`,
-					type: 'photo',
-				})
-			} else {
-				await ctx.reply(`
-					${i18n.__('comment')}:
-					${issue.comment}
+		if (ctx.session.issues) {
+			ctx.session.issues.forEach(async issue => {
+				if (issue.photo) {
+					mediaGroup.push({
+						media: { source: `images/${issue.photo}` },
+						caption: `
+						${i18n.__('comment')}:
+						${issue.comment}
+						`,
+						type: 'photo',
+					})
+				} else {
+					await ctx.reply(`
+						${i18n.__('comment')}:
+						${issue.comment}
+	
+						${i18n.__('Prompts.Issue.noPhoto')}
+					`)
+				}
+			})
 
-					${i18n.__('Prompts.Issue.noPhoto')}
-				`)
-			}
-		})
-
-		await ctx.replyWithMediaGroup(mediaGroup)
-		await ctx.reply('🤖', homeBtn())
+			await ctx.replyWithMediaGroup(mediaGroup)
+			await ctx.reply('🤖', homeBtn())
+		} else {
+			ctx.reply('0️⃣', homeBtn())
+		}
 	})
 
 	bot.command(Commands.LOCATION, async ctx => {
